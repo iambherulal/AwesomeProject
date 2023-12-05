674328c3edf7a0dc4e1b2a9905d68f5c1f1dfe05
Bheru Lal Gameti <bherulalgameti24@gmail.com>
2023-12-05 12:11:28 +0530

    intial commit

---
+++ App.tsx
@@ -0,0 +1,133 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+  Image,
+} from 'react-native';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = 'bg-white dark:bg-slate-900';
+  const people = [
+    {
+      name: 'Leslie Alexander',
+      email: 'leslie.alexander@example.com',
+      role: 'Co-Founder / CEO',
+      imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: '3h ago',
+      lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+      name: 'Michael Foster',
+      email: 'michael.foster@example.com',
+      role: 'Co-Founder / CTO',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: '3h ago',
+      lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+      name: 'Dries Vincent',
+      email: 'dries.vincent@example.com',
+      role: 'Business Relations',
+      imageUrl:
+        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: null,
+    },
+    {
+      name: 'Lindsay Walton',
+      email: 'lindsay.walton@example.com',
+      role: 'Front-end Developer',
+      imageUrl:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: '3h ago',
+      lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+      name: 'Courtney Henry',
+      email: 'courtney.henry@example.com',
+      role: 'Designer',
+      imageUrl:
+        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: '3h ago',
+      lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+      name: 'Tom Cook',
+      email: 'tom.cook@example.com',
+      role: 'Director of Product',
+      imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: null,
+    },
+  ];
+  return (
+    <SafeAreaView className={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        className={backgroundStyle}>
+        <View role="list" className="divide-y divide-gray-100">
+          {people.map(person => (
+            <View
+              key={person.email}
+              className="flex justify-between gap-x-6 py-5">
+              <View className="flex min-w-0 gap-x-4">
+                <Image
+                  className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                  source={{uri: person.imageUrl}}
+                />
+                <View className="min-w-0 flex-auto">
+                  <Text className="text-xl font-semibold leading-6 text-gray-900">
+                    {person.name}
+                  </Text>
+                  <Text className="mt-1 truncate text-xs leading-5 text-gray-500">
+                    {person.email}
+                  </Text>
+                </View>
+              </View>
+              <View className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                <Text className="text-sm leading-6 text-gray-900">
+                  {person.role}
+                </Text>
+                {person.lastSeen ? (
+                  <Text className="mt-1 text-xs leading-5 text-gray-500">
+                    Last seen
+                  </Text>
+                ) : (
+                  <View className="mt-1 flex items-center gap-x-1.5">
+                    <View className="flex-none rounded-full bg-emerald-500/20 p-1">
+                      <View className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                    </View>
+                    <Text className="text-xs leading-5 text-gray-500">
+                      Online
+                    </Text>
+                  </View>
+                )}
+              </View>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
